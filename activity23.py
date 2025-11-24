644e7ba91aa5521b40127a4a1fba3ff26c1f99fa
Shan <deguzmanshahin@gmail.com>
2025-11-24 15:29:50 +0800

    commit py23

---
+++ activity23.py
@@ -0,0 +1,18 @@
+fruit1 = 'Hi'
+fruit2 = 'hello'
+
+fruits =['mangoes, apples, lemons, strawberry']
+            #0        #1      #2        #3
+print(fruits)
+fruits.append("starfruit")
+print(fruits)
+fruits.insert(2, "guava")
+print(fruits)
+fruits.pop()
+print(fruits)
+fruits.remove('starfruit')
+print(fruits)
+
+print(len("fruits"))
+fruits.sort()
+print(fruits)
