805cab0a170fd828d571a3faeef6d55072acae91
Shan <deguzmanshahin@gmail.com>
2025-11-24 15:34:22 +0800

    commit py25

---
+++ activity25.py
@@ -0,0 +1,7 @@
+programming_language = ['Java', 'Python', 'CSS',]
+print(programming_language[-1])
+
+programming_dictionary = {'pang matanda':'HELLO'}
+print(programming_dictionary['pang matanda'])
+
+
