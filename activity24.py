194126be3607645dd96337f404af640efcf5c0a1
Shan <deguzmanshahin@gmail.com>
2025-11-24 15:35:11 +0800

    commit py24

---
+++ activity24.py
@@ -0,0 +1,13 @@
+from activity24_1 import ShanWOW, ShanWEW
+
+
+def ShahinwOw():
+    print("Hi, Shahin! How you doing today?")
+    print("Welcome to Activity24")
+
+ShahinwOw()
+ShanWOW("Shahin")
+ShanWOW("Perez")
+ShanWEW(input("Your name "), input("Your location "), input("Your age "))
+
+
